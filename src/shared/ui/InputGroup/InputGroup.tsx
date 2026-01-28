97e9dafec1227c40a70358456017253c8be6c832
강동욱 <ehddnr8563@gmail.com>
2026-01-29 05:20:51 +0900

    feat: InputGroup, Input 공통 컴포넌트 구현

---
+++ src/shared/ui/InputGroup/InputGroup.tsx
@@ -0,0 +1,22 @@
+import { cn } from '@/shared/lib/utils';
+
+import * as React from 'react';
+
+const InputGroup = React.forwardRef<
+  HTMLDivElement,
+  React.HTMLAttributes<HTMLDivElement>
+>(({ className, ...props }, ref) => (
+  <div
+    ref={ref}
+    className={cn(
+      'flex w-full items-center overflow-hidden transition-all',
+      'rounded-full border border-gray-200 bg-white pl-4', // 기본 보더 & 라운드
+      'focus-within:border-brand-500/50 focus-within:ring-brand-500/20 focus-within:ring-1', // 포커스 시 효과
+      className
+    )}
+    {...props}
+  />
+));
+InputGroup.displayName = 'InputGroup';
+
+export default InputGroup;
---
+++ src/shared/ui/InputGroup/index.ts
@@ -0,0 +1,3 @@
+export { default as InputGroup } from './InputGroup';
+export { default as InputGroupAddon } from './InputGroupAddon';
+export { default as InputGroupInput } from './InputGroupInput';
